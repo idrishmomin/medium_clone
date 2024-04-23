6ffedfcf40500b93fd86fd6c03424a71c809e0ff
Vikas <vikasaggarwal3213@gmail.com>
2024-04-23 09:57:31 +0530

    add frontend code

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,10 @@
+export const Quote = () => {
+    return <div className="bg-slat-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-md text-center bg-red-200">
+                Hi My self Software Engineer.
+            </div>
+        </div>
+    </div>
+}
+
---
+++ frontend/src/pages/SignUp.tsx
@@ -0,0 +1,16 @@
+import { Quote } from "../components/Quote"
+
+export const SignUp = () => {
+    return <div>
+
+        <div className="display flex">
+            <div>
+                test
+            </div>
+            <div>
+                <Quote />
+            </div>
+        </div>
+
+    </div>
+}
