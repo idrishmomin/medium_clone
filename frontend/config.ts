bd3e471f17dfb27a3cfd7d586e587577d20ed141
Vikas <vikasaggarwal3213@gmail.com>
2024-04-25 10:15:18 +0530

    add signup and signin block

---
+++ frontend/config.ts
@@ -0,0 +1,1 @@
+export const Backend_BASE_URL = "http://localhost:3000";
--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,6 +1,9 @@
 import { SignupInput } from "@idrishmomin/zodcommon";
+import axios from "axios";
 import { ChangeEvent, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Backend_BASE_URL } from "../config";
+
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
 
@@ -10,6 +13,10 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
         name: ""
     });
 
+    async function sendRequestToBackend () {
+        const response = await axios.post(`${Backend_BASE_URL}/api/v1/user/signup`).then
+
+    }
     return <div className="h-screen flex justify-center flex-col ">
 
         <div className="flex justify-center">
@@ -19,18 +26,20 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                         Create an Account
                     </div>
                     <div className="text-slate-400">
-                        Already have an account?
-                        <Link className="underline pl-2" to={"/signin"}>Login</Link>
+                        {type === "signin" ? "Dont have an account?" : "Already have an account?"}
+                        <Link className="underline pl-2" to={type === "signin" ? "/signup" : "/signin"}>
+                            {type === "signin" ? "Sign up" : "Sign in"}
+                        </Link>
                     </div>
                 </div>
 
                 <div>
-                    <LabeledInput label="UserName" placeholder="Enter username" onChange={(e) => {
+                    {type === "signup" ? <LabeledInput label="UserName" placeholder="Enter username" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             name: e.target.value
                         })
-                    }} />
+                    }} /> : null}
 
                     <LabeledInput label="Email" placeholder="m@example.com" onChange={(e) => {
                         setPostInputs({
@@ -62,11 +71,8 @@ interface LabeledInputType {
 
 function LabeledInput({ label, placeholder, onChange, type }: LabeledInputType) {
     return <div>
-        <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}  </label>
-            <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 
-            text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 
-            dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
-        </div>
+        <label className="block mb-2 text-sm font-medium text-black pt-1">{label}  </label>
+        <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 
+            text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder={placeholder} required />
     </div>
 } 
